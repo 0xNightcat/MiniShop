285d33cb8d506b64ca8182f101f9db9bba7e5a3f
Behnam <nightcat.sec@gmail.com>
2023-09-22 16:00:51 +0330

    newest products fetched

--- src/containers/Home/Main/NewProducts/NewProducts.js
+++ src/containers/Home/Main/NewProducts/NewProducts.js
@@ -1,19 +1,46 @@
-import React from 'react';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { Container } from 'react-bootstrap';
+import { homeAction } from '../../../../action/homeAction';
 
 import './NewProducts.scss';
 
-import TrendProduct from '../TrendProducts/TrendProduct/TrendProduct';
+import NewProduct from './NewProduct/NewProduct';
 
-function TrendProducts() {
+function NewProducts() {
+  const dispatch = useDispatch();
+  
+  // get states
+  const home = useSelector((state) => state.home);
+  const { products } = home;
+
+  useEffect(() => {
+    dispatch(homeAction());
+  }, [])
+
+  // sort products by more sale
+  products.sort((a, b) => {
+    if ( a.date > b.date ){
+      return -1;
+    }
+    if ( a.date < b.date ){
+      return 1;
+    }
+    return 0;
+  });
+
+  console.log(products);
+
+  const newestPros = products.slice(0, 4); // limit products count
+
   return (
     <section className='new_products mt-5'>
       <Container>
          <h4 className='title text-center'>جدیدترین محصولات</h4>
-            <TrendProduct />
+            <NewProduct Newprods={newestPros} />
       </Container>
     </section>
   )
 }
 
-export default TrendProducts;
+export default NewProducts;
